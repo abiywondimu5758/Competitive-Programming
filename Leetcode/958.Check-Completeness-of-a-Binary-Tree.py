2f040138414a01f0af164e464dd528ce1a66ce8a
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2023-10-18 12:51:06 +0300

    Update 958.Check-Completeness-of-a-Binary-Tree.py

--- Leetcode/958.Check-Completeness-of-a-Binary-Tree.py
+++ Leetcode/958.Check-Completeness-of-a-Binary-Tree.py
@@ -1,1 +1,21 @@
-
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True        
+        queue = [root]
+        null_seen = False
+        while queue:
+            node = queue.pop(0)
+            if not node:
+                null_seen = True
+            else:
+                if null_seen:
+                    return False
+                queue.extend([node.left,node.right])
+        return True
