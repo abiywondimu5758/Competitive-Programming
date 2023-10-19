c71391df302d4c33429cd344bc765b4e462f7a04
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2023-10-19 21:03:58 +0300

    Create 589. N-ary Tree Preorder Traversal.py

---
+++ Leetcode/589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        arr = []
+        def postorder(root):
+            nonlocal arr
+            if not root: return None
+            arr.append(root.val)
+            for i in root.children:
+                postorder(i)
+        postorder(root)
+        return arr
+        
