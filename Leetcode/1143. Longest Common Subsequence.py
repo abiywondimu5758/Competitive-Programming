09f0ffcf40c73bfd7c9966b7cc4cde1b3c97c6d6
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2023-10-27 10:43:23 +0300

    Update 1143. Longest Common Subsequence.py

--- Leetcode/1143. Longest Common Subsequence.py
+++ Leetcode/1143. Longest Common Subsequence.py
@@ -1,1 +1,16 @@
-
+#Solution using dictionary
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {}
+        def recursion(t1:int,t2:int):
+            key = t1,t2
+            if key in memo:
+                return memo[key]
+            elif t1 == len(text1) or t2 == len(text2):
+                memo[key] = 0
+            elif text1[t1] == text2[t2]:
+                memo[key] = 1 + recursion(t1 + 1, t2 + 1)
+            else:
+                memo[key] = max(recursion(t1 + 1, t2), recursion(t1, t2 + 1))
+            return memo[key]
+        return recursion(0,0)
