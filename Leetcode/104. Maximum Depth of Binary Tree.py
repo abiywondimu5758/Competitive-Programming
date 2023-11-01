e12846387a0337f4b9864401c3665b19facaf636
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2023-11-01 17:48:03 +0300

    Update 104. Maximum Depth of Binary Tree.py

--- Leetcode/104. Maximum Depth of Binary Tree.py
+++ Leetcode/104. Maximum Depth of Binary Tree.py
@@ -4,6 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Iterative
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        stack = [(root,1)]
+        maxDepth = 0
+        while stack:
+            root, depth = stack.pop()
+            maxDepth = max(x,depth)
+            if root.left:
+                stack.append((root.left,depth + 1))
+            if root.right:
+                stack.append((root.right,depth + 1))
+        return maxDepth
+
+# Recursive
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         def md(root):
