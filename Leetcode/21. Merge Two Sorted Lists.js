717505c43381661bf5f00f94609f24fb622e90f0
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-02-16 20:13:05 +0300

    Create 21. Merge Two Sorted Lists.js

---
+++ Leetcode/21. Merge Two Sorted Lists.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+// Iterative Solution
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(-1);  
+    let tail = dummy;
+    current1 = list1
+    current2 = list2
+    while(current1 != null && current2 != null){
+        if(current1.val <= current2.val){
+            tail.next = current1
+            current1 = current1.next
+        }
+        else{
+            tail.next = current2
+            current2 = current2.next  
+        }
+        tail = tail.next
+    }
+    if(current1 != null) {
+        tail.next = current1;
+    } else {
+        tail.next = current2;
+    }
+    return dummy.next;
+};
+
+// Recursive Solution
