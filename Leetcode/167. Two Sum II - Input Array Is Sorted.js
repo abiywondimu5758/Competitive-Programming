582592d2b662cb17e435755354598fa8b15180b8
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-07 20:50:22 +0300

    Update 167. Two Sum II - Input Array Is Sorted.js

--- Leetcode/167. Two Sum II - Input Array Is Sorted.js
+++ Leetcode/167. Two Sum II - Input Array Is Sorted.js
@@ -1,1 +1,21 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    pointer1 = 0
+    pointer2 = numbers.length-1
 
+    while(pointer1 < pointer2){
+    if(numbers[pointer1] + numbers[pointer2] > target){
+        pointer2--;
+    }
+    else if(numbers[pointer1] + numbers[pointer2] < target){
+        pointer1++
+    }
+    else{
+        return[pointer1+1,pointer2+1]
+    }
+    }
+};
