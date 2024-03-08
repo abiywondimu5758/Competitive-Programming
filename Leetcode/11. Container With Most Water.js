b8e429870d3297dbf273f545c0ae595211ab3f42
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-08 12:11:38 +0300

    Update 11. Container With Most Water.js

--- Leetcode/11. Container With Most Water.js
+++ Leetcode/11. Container With Most Water.js
@@ -1,1 +1,26 @@
 
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let pointer1 = 0
+    let pointer2 = height.length - 1
+    let maxArea = 0
+    while(pointer1 < pointer2){
+        area = (pointer2 - pointer1) * Math.min(height[pointer1], height[pointer2])
+        maxArea = Math.max(area,maxArea)
+        if(height[pointer1]<height[pointer2]){
+            pointer1++;
+        }
+        else if(height[pointer1]>height[pointer2]){
+            pointer2--;
+        }
+        else{
+            pointer1++;
+            pointer2--;
+        }
+
+    }
+    return maxArea;
+};
