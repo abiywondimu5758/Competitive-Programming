e11e8419d06a9c1758fcb39d4a85204ef82d4278
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-08 10:01:27 +0300

    Update 125. Valid Palindrome.js

--- Leetcode/125. Valid Palindrome.js
+++ Leetcode/125. Valid Palindrome.js
@@ -1,1 +1,20 @@
-
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    let pointer1=0
+    let pointer2=str.length-1;
+    while(pointer1<=pointer2){
+        if(str[pointer1]==str[pointer2]){
+            pointer1++;
+            pointer2--;
+        }
+        else{
+            return false
+        }
+    
+    }
+    return true
+};
