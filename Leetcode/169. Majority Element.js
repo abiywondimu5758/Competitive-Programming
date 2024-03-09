95f2048b0e0461f36ca51990fb2b822d1f09e8c6
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-09 20:27:11 +0300

    Update 169. Majority Element.js

--- Leetcode/169. Majority Element.js
+++ Leetcode/169. Majority Element.js
@@ -1,1 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let map = new Map()
 
+    for(let num of nums){
+        map.set(num, (map.get(num) || 0) +1 )
+    }
+
+    for(let[num,freq] of map){
+        if(freq > (nums.length/2)){
+            return num
+        }
+    }
+};
