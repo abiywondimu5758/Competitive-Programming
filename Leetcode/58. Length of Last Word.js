cfd6438ea5c69125ff5da3a3fc4640714c7f7ad9
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-11 19:32:26 +0300

    Update 58. Length of Last Word.js

--- Leetcode/58. Length of Last Word.js
+++ Leetcode/58. Length of Last Word.js
@@ -1,1 +1,9 @@
-
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let t = s.trim()
+    let words = t.split(' ')
+    return words[words.length-1].length
+};
