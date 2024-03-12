36c0b7096f4c10074ba1c0040cfb828733ee0bfc
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-12 19:09:10 +0300

    Update 3. Longest Substring Without Repeating Characters.js

--- Leetcode/3. Longest Substring Without Repeating Characters.js
+++ Leetcode/3. Longest Substring Without Repeating Characters.js
@@ -1,1 +1,20 @@
 
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let counter = 0
+    let map = new Map()
+    let left = 0
+
+    for(let right = 0; right < s.length; right++){
+        let currentChar = s[right]
+        if(map.has(currentChar) && map.get(currentChar) >= left){
+            left = map.get(currentChar) + 1
+        }
+        map.set(currentChar,right)
+        counter = Math.max(counter, right - left + 1)
+    }
+    return counter;
+};
