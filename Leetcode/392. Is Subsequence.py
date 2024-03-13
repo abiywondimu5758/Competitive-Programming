059e2c378265a6c4129bdb168ed46527c3f95575
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-13 19:23:06 +0300

    Update 392. Is Subsequence.py

--- Leetcode/392. Is Subsequence.py
+++ Leetcode/392. Is Subsequence.py
@@ -6,3 +6,24 @@ class Solution:
                 i += 1
             j += 1
         return i==len(s)
+# JS
+# /**
+#  * @param {string} s
+#  * @param {string} t
+#  * @return {boolean}
+#  */
+# var isSubsequence = function(s, t) {
+#     let pointer1 = 0
+#     let pointer2 = 0
+
+#     while(pointer2 < t.length){
+#         if(s[pointer1] === t[pointer2]){
+#             pointer1++;
+#             pointer2++;
+#         }
+#         else {
+#             pointer2++; 
+#         }
+#     }
+#     return pointer1 === s.length
+# };
