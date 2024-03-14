08ddd1c540ebe4b1b404e46688a233cd3a8c7e4f
Abiy Wondimu Hailemariam <56735280+abiywondimu5758@users.noreply.github.com>
2024-03-14 20:27:49 +0300

    Update 383. Ransom Note.js

--- Leetcode/383. Ransom Note.js
+++ Leetcode/383. Ransom Note.js
@@ -1,1 +1,25 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let ransomNoteMap = new Map();
+    let magazineMap = new Map();
 
+    for(let char of ransomNote){
+        ransomNoteMap.set(char, (ransomNoteMap.get(char) || 0) +1);
+    }
+    for(let char of magazine){
+        magazineMap.set(char, (magazineMap.get(char) || 0) +1);
+    }
+
+    for( let [char, count] of ransomNoteMap){
+        if(!magazineMap.has(char) || magazineMap.get(char) < count){
+            return false;
+        }
+    }
+
+    return true
+
+};
